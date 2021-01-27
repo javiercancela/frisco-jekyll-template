c0cd46de05fa4d065427092aeaf529c5b0d559b0
Javier Cancela <javiercancela@users.noreply.github.com>
2021-01-27 14:14:39 +0100

    Changes

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,8 @@ git add -A .
 git commit -m "Changes"
 git push origin release
 rm -rf ../site
-
+mkdir ../site
+cd ../site
+git clone -b master https://github.com/javiercancela/javiercancela.github.io.git
+cd javiercancela.github.io/
+rm -rf ./*
